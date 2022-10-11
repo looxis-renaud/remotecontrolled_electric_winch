b4d3f2e27602397f9fcb720d5c6470905422d585
looxis-renaud <etienne.renaud@looxis.de>
2022-10-11 16:37:44 +0200

    updated display code
    I could not make Robert Zach's code work with the ESP32 Paxcounter Board that is linked in the readme.md, so I dug into it and found a solution. updated the code accordingly. I used an updated SSD1306 Library

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -7,14 +7,25 @@
  */
 // communication is locked to a specific transmitter for 5 seconds after his last message
 // admin ID 0 can allays take over communication
-static int myID = 1;    // set to your desired transmitter id!!! [unique number from 1 - 15]
-static int myMaxPull = 75;  // 0 - 127 [kg], must be scaled with VESC ppm settings
+static int myID = 8;    // set to your desired transmitter id!!! [unique number from 1 - 15]
+static int myMaxPull = 85;  // 0 - 127 [kg], must be scaled with VESC ppm settings
 
 #include <Pangodream_18650_CL.h>
 #include <SPI.h>
 #include <LoRa.h>
-#include <Wire.h>  
-#include "SSD1306.h"
+// #include <Wire.h>  // replaced with input from SSD1306DrawingDemo.ino
+// #include "SSD1306.h" // replaced with input from SSD1306DrawingDemo.ino
+
+// Etienne: the following is taken from SSD1306DrawingDemo.ino:
+// Include the correct display library
+// For a connection via I2C using Wire include
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
+
+// Initialize the OLED display using Wire library
+SSD1306Wire display(0x3c, SDA, SCL);   // ADDRESS, SDA, SCL - SDA and SCL usually populate automatically based on your board's pins_arduino.h e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
+// End stuff taken from SS1306DrawingDemo.io
+
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISnO
 #define MOSI    27   // GPIO27 -- SX1278's MOSI
@@ -23,7 +34,7 @@ static int myMaxPull = 75;  // 0 - 127 [kg], must be scaled with VESC ppm settin
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND  868E6  //frequency in Hz (433E6, 868E6, 915E6) 
 
-SSD1306 display(0x3c, 21, 22);
+// SSD1306 display(0x3c, 21, 22); //replaced with the above from SSD1306DrawingDemo.ino
 int rssi = 0;
 float snr = 0;
 String packSize = "--";
@@ -80,12 +91,12 @@ unsigned long loraErrorMillis = 0;
 void setup() {
   Serial.begin(115200);
   
-  //OLED display
-  pinMode(16,OUTPUT);
-  pinMode(2,OUTPUT);
-  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
-  delay(50); 
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+ // //OLED display // replaced with stuff from SSD1306DrawingDemo.ino
+ // pinMode(16,OUTPUT);
+ // pinMode(2,OUTPUT);
+ // digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+ // delay(50); 
+ // digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
 
   //lora init
   SPI.begin(SCK,MISO,MOSI,SS);
